a48fd0c7f3791e117929869b622810238c53bd66
triantadim <d.trianta3@gmail.com>
2020-10-29 12:35:29 +0200

    new change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
-// Your First Program
+ // Your First Program
 
 class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("Hello, World!1"); 
+        System.out.println("Hello, World!2"); 
         System.out.println("Hello, World!"); 
     }
 }
